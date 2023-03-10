4c6b9bbf0b7b980d4ce468e1343d65117a1cab9b
karina silva <99703389+Haaksilva@users.noreply.github.com>
2023-03-10 19:39:20 -0300

    Update App.java

--- GestaoRH/src/App.java
+++ GestaoRH/src/App.java
@@ -1,9 +1,37 @@
-import java.math.BigDecimal;
 
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        var Engenheiro= new engenheiro(
-           
+public class Main {
+    public static void main(String[] args) {
+
     }
+    private String Funcionario; 
+    private int codigo;
+
+
+    public Main(String Funcionario, int codigo) {
+        this.Funcionario = funcionario;
+        this.codigo = codigo;
+    }
+
+    public String getFuncionario() {
+        return funcionario;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+
+
+
+    public int getCota() {
+        return cota;
+    }
+
+
+
+
+
+    private int cota;
+
+
 }
